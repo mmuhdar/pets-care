e6ce1d69183ab77d55c4173f5e8edccd2c854635
mmuhdar <mmuhdar17@gmail.com>
2021-07-30 06:34:06 +0700

    setutp

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 "use strict";
-
+require("dotenv").config();
 const express = require("express")
 const app = express()
 const router = require("./routes/index")
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.set("view engine", "ejs")
 app.use(express.urlencoded({extended:true}))
